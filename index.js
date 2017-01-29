3d371704cd7934971c019fbe54bdf49c56fb68d1
Argha Sarkar <argha@arghasarkar.co.uk>
2017-01-29 08:56:26 +0000

    feat(PusherFeed): Working with pusher feed (#1)
    - Able to append to feed.
    - Able to retrieve items from feed.
    
    [QA]
    Has tests: No
    Reviewed by: Nil

---
+++ index.js
@@ -0,0 +1,56 @@
+var APP_ID = "6cc6df33-1f71-48d0-9868-e1de48b64467";
+
+var app = new PusherPlatform.App({
+    appId: '6cc6df33-1f71-48d0-9868-e1de48b64467',
+});
+
+console.log(app);
+
+var myFeed = app.feed('hackathonIdeas');
+
+load();
+
+function addNewIdeaToList(newIdea, SEND_AGAIN) {
+    var newIdea = document.getElementById("newIdea").value;
+
+    if (SEND_AGAIN) {
+        myFeed.append(newIdea)
+            .then(function(response) {
+                console.log("Success" + newIdea);
+                console.log(response)
+            })
+            .catch(err => console.error('Error:', err));
+
+        console.log("Idea: " + newIdea);
+
+        myFeed.append({ yourKey: APP_ID, message: newIdea})
+            .then(response => console.log('Success:', response))
+            .catch(err => console.error('Error:', err));
+    }
+
+
+}
+
+function newItem(newItem) {
+    console.log(typeof newItem)
+
+    if (typeof newItem === "string") {
+        var newTextNode = document.createTextNode(newItem);
+
+        var newIdeaLi = document.createElement("li");
+        newIdeaLi.appendChild(newTextNode);
+        document.getElementById("ideasList").appendChild(newIdeaLi);
+    }
+
+}
+
+function load() {
+
+    myFeed.subscribe({
+        lastEventId: "25",
+        onOpen: () => console.log('Connection established'),
+        onItem: item =>  newItem(item.body),
+        onError: error => console.error('Error:', error),
+    });
+
+}
