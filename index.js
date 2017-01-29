9a5ed452bcf4c3e80a6199a9761ce783d7801bd2
Argha Sarkar <argha.sarkar1994@gmail.com>
2017-01-29 10:35:16 +0000

    Fixed the issues

--- index.js
+++ index.js
@@ -4,26 +4,29 @@ var app = new PusherPlatform.App({
     appId: '6cc6df33-1f71-48d0-9868-e1de48b64467',
 });
 
-console.log(app);
-
 var myFeed = app.feed('recipeResearch');
 
 load();
 
-function addNewIdeaToList(newIdea, SEND_AGAIN) {
-    var newIdea = document.getElementById("newIdea").value;
+function addNewIdeaToList(newRecipeName,  SEND_AGAIN) {
+
+    var newRecipeName = document.getElementById("newRecipe").value;
+    var newRecipeLink = document.getElementById("recipeLink").value;
+
+    var newRecipe = {
+        name: newRecipeName,
+        link: newRecipeLink
+    };
 
     if (SEND_AGAIN) {
-        myFeed.append(newIdea)
+        myFeed.append(newRecipe)
             .then(function(response) {
-                console.log("Success" + newIdea);
+                console.log("Success" + newRecipe);
                 console.log(response)
             })
             .catch(err => console.error('Error:', err));
 
-        console.log("Idea: " + newIdea);
-
-        myFeed.append({ yourKey: APP_ID, message: newIdea})
+        myFeed.append({ yourKey: APP_ID, message: newRecipe})
             .then(response => console.log('Success:', response))
             .catch(err => console.error('Error:', err));
     }
@@ -32,14 +35,31 @@ function addNewIdeaToList(newIdea, SEND_AGAIN) {
 }
 
 function newItem(newItem) {
-    console.log(typeof newItem)
 
-    if (typeof newItem === "string") {
-        var newTextNode = document.createTextNode(newItem);
+    console.log(JSON.stringify(newItem));
 
-        var newIdeaLi = document.createElement("li");
-        newIdeaLi.appendChild(newTextNode);
-        document.getElementById("ideasList").appendChild(newIdeaLi);
+    if (typeof newItem.name === "string" && typeof newItem.link === "string") {
+        var recipeName = newItem.name;
+        var recipeLink = newItem.link;
+
+        var recipeNameTextNode = document.createTextNode("Name: " + recipeName + "    ");
+        var createLink = document.createElement("a");
+        createLink.href = recipeLink;
+        createLink.text = "Link: " + recipeLink;
+        createLink.style.marginLeft = "30px;";
+
+        var createListElement = document.createElement("li");
+        createListElement.appendChild(recipeNameTextNode);
+        createListElement.appendChild(createLink);
+
+        document.getElementById("recipeList").appendChild(createListElement);
+
+        // Delete textbox content
+        document.getElementById("newRecipe").value = "";
+        document.getElementById("recipeLink").value = "";
+
+        console.log("Name: " + recipeName);
+        console.log("Link: " + recipeLink);
     }
 
 }
